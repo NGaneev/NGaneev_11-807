cd052b0a0f0a713ab8c793e6e378643a76553f8a
Niyaz Ganeev <NFGaneev@stud.kpfu.ru>
2018-09-15 10:34:33 +0300

    task1 HW03 is completed

---
+++ untitled.git/src/com/company/task5.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class task5 {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 2, 11, 133};
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
