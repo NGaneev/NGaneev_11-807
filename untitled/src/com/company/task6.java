cea435161dd2e82d75dd65f695a1716c7343f1ba
Niyaz Ganeev <NFGaneev@stud.kpfu.ru>
2018-09-16 18:27:20 +0300

    task6 HW03 is completed

--- untitled/src/com/company/task6.java
+++ untitled/src/com/company/task6.java
@@ -3,18 +3,17 @@ package com.company;
 import java.util.Scanner;
 
 public class task6 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Количество элементов: ");
+        System.out.print("Введите количество чисел: ");
         int n = sc.nextInt();
-        System.out.print("Введите элементы: ");
+        System.out.print("Введите элементы массива: ");
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i = n; i > n; i--) {
-            System.out.println(arr[i]);
+        for (int i = n-1; i > -1; i--) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
