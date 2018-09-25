08b6b167e03406787d9776a82a8c8e0852b98ba7
Niyaz Ganeev <NFGaneev@stud.kpfu.ru>
2018-09-25 11:45:48 +0300

    task5 is completed

---
+++ HW06/task5HW6.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class task5HW6 {
+    public static void main(String[] args) {
+        int count = 0;
+        System.out.print("Введите количество элементов первого массива: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        //char[] arr1Char = Integer.toString(arr1).toCharArray();
+        System.out.print("Введите элементы первого массива: ");
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        System.out.print("Введите количество элементов второго массива: ");
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        int count2 = m + n;
+        //char[] arr2Char = Integer.toString(arr2).toCharArray();
+        System.out.print("Введите элементы второго массива: ");
+        for (int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr1[i] == arr2[j]) {
+                    count++;
+                }
+            }
+        }
+        count2 = count2 - 2 * count;
+        System.out.print(count2);
+    }
+}
