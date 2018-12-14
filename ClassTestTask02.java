9b9d3fba3680ca3692d2fe46d94d667a0db4ff0a
NGaneev <NFGaneev@stud.kpfu.ru>
2018-12-14 12:24:20 +0300

    Delete ClassTestTask02.java

--- ClassTestTask02.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class ClassTestTask02 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n = sc.next();
-        System.out.print(n);
-    }
-}
