453ff58807349402713d3ab735da97930650525c
NGaneev <NFGaneev@stud.kpfu.ru>
2018-12-14 12:25:08 +0300

    Delete TextAnalyzer.java

--- TextAnalyzer.java
+++
@@ -1,8 +0,0 @@
-public interface TextAnalyzer {
-    /**
-     * Counts the similarity between texts, provided by TextProviders
-     *
-     * @return coefficient of similarity
-     */
-    double analyze(TextProvider te1, TextProvider te2);
-}
